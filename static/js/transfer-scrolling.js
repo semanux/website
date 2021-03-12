d2f641ad0f79abf0d419f7558c2b53930195fd77
Raphael Menges <unrealer2@yahoo.de>
2021-03-12 11:14:52 +0100

    Overengineering language switch.

---
+++ static/js/transfer-scrolling.js
@@ -0,0 +1,12 @@
+/* Get scroll offset from URL parameters and apply it */
+const urlParams = new URLSearchParams(window.location.search);
+const scrollOffset = urlParams.get('s');
+window.scrollTo(0, scrollOffset);
+
+/* Adapt language switches to call URL with scroll offset as parameter */
+var languageSwitches = document.getElementsByName('language_switch');
+for (let item of languageSwitches) {
+    href = item.href;
+    item.setAttribute('href', '#');
+    item.setAttribute('onclick', 'window.open("' + href + '?s=\"+Math.floor(window.scrollY)+\"", "_self");');
+}
