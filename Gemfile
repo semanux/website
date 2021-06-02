b449a56af018e06fe00b30a27bc4822816f830d3
Lukas Schmelzeisen <me@lschmelzeisen.com>
2021-06-02 15:13:45 +0200

    Add prettier as auto-formatting tool and format all code.

--- Gemfile
+++ Gemfile
@@ -2,12 +2,15 @@
 
 source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| 'https://github.com/#{repo_name}' }
+git_source(:github) { |repo_name| 'https://github.com/#{repo_name}' }
 
 # gem 'rails'
 gem 'jekyll', '~> 4.2'
 gem 'wdm', '>= 0.1.0' if Gem.win_platform? # recommendation by Jekyll
-gem 'eventmachine', '1.2.7', git: 'https://github.com/eventmachine/eventmachine.git', tag: 'v1.2.7' # for livereload
+gem 'eventmachine', # for livereload
+    '1.2.7',
+    git: 'https://github.com/eventmachine/eventmachine.git',
+    tag: 'v1.2.7'
 group :jekyll_plugins do
   gem 'jekyll-polyglot', '~> 1.4'
 end
--- static/js/acryl.js
+++ static/js/acryl.js
@@ -1,15 +1,15 @@
 /* Check whether backdrop filter is supported */
-var bdFilterSupport = CSS.supports('backdrop-filter', 'blur(10px)');
+var bdFilterSupport = CSS.supports("backdrop-filter", "blur(10px)");
 
 /* If backdrop filter is supported, edit each element to be transparent and to have backdrop filter */
-if(bdFilterSupport) {
+if (bdFilterSupport) {
   var re = /(rgb)\(([0-9]+),\s+([0-9]+),\s+([0-9]+)/; // TODO: what does happen if RGBA color is input?
-  var elems = document.getElementsByClassName('acryl');
-  for(i = 0; i < elems.length; i++) {
+  var elems = document.getElementsByClassName("acryl");
+  for (i = 0; i < elems.length; i++) {
     let color = window.getComputedStyle(elems[i]).backgroundColor;
-    var subst = 'rgba($2,$3,$4,0.85'; 
+    var subst = "rgba($2,$3,$4,0.85";
     color = color.replace(re, subst);
     elems[i].style.backgroundColor = color;
-    elems[i].style.backdropFilter = 'blur(10px)';
+    elems[i].style.backdropFilter = "blur(10px)";
   }
 }
